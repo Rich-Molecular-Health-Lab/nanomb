054290f71421c913a026ef0d34e7db12f5112fd3
aliciamrich <aliciamrich@gmail.com>
2025-10-09 23:19:40 -0500

    please work this time

--- docker/cpu/build_cpu.sh
+++ docker/cpu/build_cpu.sh
@@ -3,9 +3,15 @@ docker build \
   -t nanomb:0.2.0-cpu \
   -f docker/cpu/Dockerfile .
   
-# optional: sanity check before tagging/pushing
-docker run --burm -it nanomb:iqtree3 bash -lc "iqtree -h | head -1; isonclust3 --help | head -1"
 
-
-docker tag nanomb:iqtree3 aliciamrich/nanomb:0.2.0-cpu
 docker push aliciamrich/nanomb:0.2.0-cpu
+
+docker run --rm -it nanomb:0.2.0-cpu bash -lc '
+  echo "PATH=$PATH";
+  which python mafft NanoPlot iqtree isonclust3;
+  python --version;
+  mafft --version | head -1;
+  NanoPlot --version;
+  iqtree -h | head -1;
+  isonclust3 --help | head -1;
+'
