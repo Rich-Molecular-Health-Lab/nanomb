0399119929315c17ff8f1d8829bf529df74b4acd
aliciamrich <aliciamrich@gmail.com>
2025-10-09 23:03:17 -0500

    glitches forever

--- docker/cpu/build_cpu.sh
+++ docker/cpu/build_cpu.sh
@@ -1,8 +1,8 @@
-# 1️⃣ Build the container (with IQ-TREE 3)
 docker build \
+  --build-arg IQTREE_URL="https://github.com/iqtree/iqtree3/releases/download/v3.0.1/iqtree-3.0.1-Linux.tar.gz" \
   -t nanomb:0.2.0-cpu \
   -f docker/cpu/Dockerfile .
-
+  
 # optional: sanity check before tagging/pushing
 docker run --burm -it nanomb:iqtree3 bash -lc "iqtree -h | head -1; isonclust3 --help | head -1"
 
